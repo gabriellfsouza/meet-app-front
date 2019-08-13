8ce4dfce9df6cd267e0aacedb9a750122e9e518e
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-13 05:40:48 -0300

    feature: routing

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '~/pages/SignIn';
+import SignOut from '~/pages/SignOut';
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
+import AddEdit from '~/pages/AddEdit';
+import Details from '~/pages/Details';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignOut} />
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/details" component={Profile} />
+      <Route path="/add" component={AddEdit} />
+      <Route path="/edit" component={Details} />
+    </Switch>
+  );
+}
