f78c7abc9ee58e5dc17f1b214091223a5f3db882
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-14 06:13:42 -0300

    conf: adding redux, saga and reactotron logics to project.

--- src/pages/SignOut/index.js
+++ src/pages/SignOut/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.png';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
 
 export default function SignOut() {
   function handleSubmit(data) {
@@ -12,7 +23,7 @@ export default function SignOut() {
   return (
     <>
       <img src={logo} alt="MeetApp" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Digite seu e-mail" />
         <Input
