d752edeb8fb0c8253ee304ae33dc7286c8a217e3
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-14 04:25:42 -0300

    chore: initial styles was included

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignOut from '../pages/SignOut';
@@ -14,10 +15,12 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignOut} />
 
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/details" component={Profile} />
-      <Route path="/add" component={AddEdit} />
-      <Route path="/edit" component={Details} />
+      <Route path="/dashboard" isPrivate component={Dashboard} />
+      <Route path="/details" isPrivate component={Profile} />
+      <Route path="/add" isPrivate component={AddEdit} />
+      <Route path="/edit" isPrivate component={Details} />
+
+      <Route path="/" component={() => <h1>404</h1>} />
     </Switch>
   );
 }
