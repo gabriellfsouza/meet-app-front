352a07a5241b5f4afe79fab5447e074fbbce2a15
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-17 04:02:20 -0300

    chore: auth browser storage

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -7,14 +7,23 @@ const INITITAL_STATE = {
 };
 
 export default function auth(state = INITITAL_STATE, action) {
-  switch (action.type) {
-    case '@auth/SIGN_IN_SUCCESS': {
-      return produce(state, draft => {
+  return produce(state, draft => {
+    switch (action.type) {
+      case '@auth/SIGN_IN_REQUEST': {
+        draft.loading = true;
+        break;
+      }
+      case '@auth/SIGN_IN_SUCCESS': {
         draft.token = action.payload.token;
         draft.signed = true;
-      });
+        draft.loading = false;
+        break;
+      }
+      case '@auth/SIGN_FAILURE': {
+        draft.loading = false;
+        break;
+      }
+      default:
     }
-    default:
-      return state;
-  }
+  });
 }
