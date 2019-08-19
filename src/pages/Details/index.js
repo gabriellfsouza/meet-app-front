9840a3bc9536a82d19f3980788f210445d77c2ea
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-19 04:25:25 -0300

    chore: meetup details styling

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { MdAddCircleOutline, MdChevronRight } from 'react-icons/md';
+import {
+  MdModeEdit,
+  MdLocationOn,
+  MdEvent,
+  MdDeleteForever,
+} from 'react-icons/md';
 import DefaultButton from '~/components/DefaultButton';
-import { Container, Resume } from './styles';
+import { Container, Description } from './styles';
+
+// será substituído pelo src da imagem gravada no repositório
+import meetupPhoto from '~/assets/meetup de react native.jpg';
 
 export default function Details() {
   return (
@@ -9,18 +17,40 @@ export default function Details() {
       <header>
         <h1>Meetup de React Native</h1>
         <span>
-          <DefaultButton type="button">
-            <MdAddCircleOutline size={20} />
+          <DefaultButton className="edit" type="button">
+            <MdModeEdit size={20} />
             Editar
           </DefaultButton>
           <DefaultButton type="button">
-            <MdAddCircleOutline size={20} />
+            <MdDeleteForever size={20} />
             Cancelar
           </DefaultButton>
         </span>
       </header>
 
-      <ul>
+      <Description>
+        <img src={meetupPhoto} alt="Meetup de React Native" />
+        <p>
+          O Meetup de React Native é um evento que reúne a comunidade de
+          desenvolvimento mobile utilizando React a fim de compartilhar
+          conhecimento. Todos são convidados.
+        </p>
+        <p>
+          Caso queira participar como palestrante do meetup envie um e-mail para
+          o rganizacao@meetuprn.com.br.
+        </p>
+        <footer>
+          <span className="date">
+            <MdEvent />
+            24 de Junho, às 20h
+          </span>
+          <span className="location">
+            <MdLocationOn />
+            Rua Guilherme Gembala, 260
+          </span>
+        </footer>
+      </Description>
+      {/* <ul>
         <Resume>
           <strong>Meetup react native</strong>
           <span>
@@ -39,7 +69,7 @@ export default function Details() {
             </button>
           </span>
         </Resume>
-      </ul>
+      </ul> */}
     </Container>
   );
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -14,6 +14,10 @@ export const Container = styled.div`
     align-items: center;
     justify-content: space-between;
 
+    button.edit{
+      background: #4dbaf9;
+    }
+
     h1 {
       color: #fff;
     }
@@ -56,48 +60,77 @@ export const Container = styled.div`
     }*/
   }
 
-  ul {
-    display: flex;
-    flex-direction: column;
-    margin-top: 30px;
-    align-items: stretch;
-  }
+
 `;
 
-export const Resume = styled.li`
-  margin: 5px 0;
+export const Description = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  padding: 20px;
-  border-radius: 4px;
-  background: rgba(24, 22, 31, 0.3);
+  flex-direction: column;
 
-  strong {
-    color: #fff;
-    font-size: 16px;
-    font-weight: bold;
+  img {
+    margin: 20px 0;
+    border-radius: 4px;
   }
 
-  span {
+  p {
+    flex: 1;
+    margin-bottom: 30px;
+    color: #fff;
+    font-size: 17px;
+    line-height: 30px;
+  }
+
+  footer {
     display: flex;
-    color: ${darken(0.5, '#fff')};
-    font-size: 16px;
+    flex-direction: row;
+    span {
+      margin-right: 30px;
+      display: flex;
+      align-items: center;
+      color: ${darken(0.4, '#fff')};
+      font-size: 15px;
 
-    button {
-      margin-left: 30px;
-      border: 0;
-      background: none;
-      color: #fff;
-      transition: color 0.2s;
-
-      &:hover {
-        color: ${darken(0.05, '#fa4d69')};
-      }
-
-      &:active {
-        color: ${darken(0.5, '#fa4d69')};
+      svg {
+        margin-right: 5px;
       }
     }
   }
 `;
+
+// export const Resume = styled.li`
+//   margin: 5px 0;
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: space-between;
+//   padding: 20px;
+//   border-radius: 4px;
+//   background: rgba(24, 22, 31, 0.3);
+
+//   strong {
+//     color: #fff;
+//     font-size: 16px;
+//     font-weight: bold;
+//   }
+
+//   span {
+//     display: flex;
+//     color: ${darken(0.5, '#fff')};
+//     font-size: 16px;
+
+//     button {
+//       margin-left: 30px;
+//       border: 0;
+//       background: none;
+//       color: #fff;
+//       transition: color 0.2s;
+
+//       &:hover {
+//         color: ${darken(0.05, '#fa4d69')};
+//       }
+
+//       &:active {
+//         color: ${darken(0.5, '#fa4d69')};
+//       }
+//     }
+//   }
+// `;
